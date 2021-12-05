2d08ab493ff7c5b0e68ebed8d6257256ef0b1793
gabrielvba <gabriel.alves@ccc.ufcg.edu.br>
2021-12-05 16:11:43 -0300

    Configuração inicial

---
+++ src/app/models/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
+const filesystem = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const { ENVIRONMENT } = require('../../config/environment');
+const config = require('../../config/database')[ENVIRONMENT];
+
+const database = {};
+
+let sequelize;
+if (ENVIRONMENT === 'production') {
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  }
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+  );
+}
+
+filesystem
+  .readdirSync(__dirname)
+  .filter(
+    (file) => file.indexOf('.') !== 0
+      && file !== path.basename(__filename)
+      && file.slice(-3) === '.js',
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes,
+    );
+    database[model.name] = model;
+  });
+
+Object.keys(database).forEach((modelName) => {
+  if (database[modelName].associate) {
+    database[modelName].associate(database);
+  }
+});
+
+database.sequelize = sequelize;
+database.Sequelize = Sequelize;
+
+module.exports = database;
---
+++ src/config/environment.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const environment = {
+  ENVIRONMENT: process.env.NODE_ENV || 'development',
+  BASE_URL: process.env.BASE_URL || 'localhost' ,
+  PORT: process.env.PORT || 3030,
+  // JWT: {
+  //   expirationMinutes: process.env.JWT_EXPIRES_TIME || 20,
+  //   expirationLogin: process.env.JWT_EXPIRES_TIME_LOGIN || 24,
+  //   secret: process.env.JWT_SECRET,
+  // },
+  // EMAIL: {
+  //   email: process.env.GMAIL_EMAIL,
+  //   password: process.env.GMAIL_PASSWORD,
+  //   port: process.env.EMAIL_SMTP_PORT,
+  //   smtp: process.env.EMAIL_SMTP_HOST,
+  // },
+  // FIREBASE: {
+  //   credentials: process.env.FIREBASE_CREDENTIALS,
+  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // },
+};
+
+module.exports = environment;
